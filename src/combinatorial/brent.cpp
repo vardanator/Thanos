248de1a72fdfc3d68ea86f2551f289a82dd94540
vardanator <vardan.l.grigoryan@gmail.com>
2018-01-12 00:07:59 +0400

    fixed

--- src/combinatorial/brent.cpp
+++ src/combinatorial/brent.cpp
@@ -4,8 +4,10 @@
 #include <iostream>
 
 // using int limits to fewer elements in a sequence
-std::pair<int, int> detect_brent_cycle(const std::array<int, 8>& sequence) {
-    if (sequence.size() <= 1) return std::make_pair(-1, -1);
+std::pair<int, int> detect_brent_cycle(const std::vector<int>& sequence) {
+    if (sequence.size() <= 1) {
+        return std::make_pair(-1, -1);
+    }
     int power = 1;
     int lam = 1;
     int index = 0; // faking f(), e.g. index is f(), f(x0) is index+1, etc.
@@ -21,26 +23,33 @@ std::pair<int, int> detect_brent_cycle(const std::array<int, 8>& sequence) {
         hare = sequence[++index];
         ++lam;
     }
-    if (tortoise != hare) return std::make_pair(-1, -1); // checking the non-cyclic sequence case
+    if (tortoise != hare) {
+        return std::make_pair(-1, -1);
+    }
     // we got 'lam' at this point
 
     int mu = 0;
     tortoise = sequence[0];
-    hare = sequence[lam - 1]; // teleport hare
+    hare = sequence[lam]; // teleport hare
 
     int tortoise_index = 0;
-    int hare_index = lam - 1;
+    int hare_index = lam;
     while (tortoise != hare && hare_index < sequence.size()) {
         tortoise = sequence[++tortoise_index];
         hare = sequence[++hare_index];
         ++mu;
     }
-    if (tortoise != hare) return std::make_pair(-1, -1);
+    std::cout << lam << " " << mu << std::endl;
+    if (tortoise != hare) {
+        return std::make_pair(-1, -1);
+    }
+
     return std::make_pair(lam, mu);
 }
 
 int main() {
-    std::array<int, 8> sequence = {2, 0, 6, 3, 1, 6, 3, 1};
+    int arr[] = {2, 0, 6, 3, 1, 6, 3, 1};
+    std::vector<int> sequence(arr, arr + 8);
     std::pair<int, int> p = detect_brent_cycle(sequence);
     std::cout << "lam == " << p.first << ", mu == " << p.second << std::endl;
 }
